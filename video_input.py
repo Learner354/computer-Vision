904c4928da48ac467165b02818ba5287c5d83052
Learner354 <forlearning.354@gmail.com>
2025-09-21 07:19:54 +0500

    Create video_input.py
    python code for reading a video using cv2

---
+++ video_input.py
@@ -0,0 +1,11 @@
+import cv2
+import os
+video_path=''
+video=cv2.videoCapture(video_path)
+ret=True
+while ret:
+  ret,frame=video.read()
+  if ret:
+    cv2.imshow('frame',frame)
+    #video is 60fps, which means every frame takes 1/60 i.e. 16ms
+    cv2.waitkey(16)
