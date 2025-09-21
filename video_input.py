96244546cb67caab89c28d3d58d293ad33c132a7
Learner354 <forlearning.354@gmail.com>
2025-09-21 07:24:33 +0500

    Update video_input.py

--- video_input.py
+++ video_input.py
@@ -1,6 +1,6 @@
 import cv2
 import os
-video_path=''
+video_path='roboticarm.avi'
 video=cv2.videoCapture(video_path)
 ret=True
 while ret:
@@ -9,3 +9,7 @@ while ret:
     cv2.imshow('frame',frame)
     #video is 60fps, which means every frame takes 1/60 i.e. 16ms
     cv2.waitkey(16)
+
+
+video.release()
+cv2.destroyAllWindows()
